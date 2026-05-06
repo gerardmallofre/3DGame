93451c07ad684a40417838f88352b18ce8364ac3
gerardmallofre <gerard.mallofre@estudiantat.upc.edu>
2026-05-07 01:26:31 +0200

    hud, coin and door model and prefab, knight fbx

--- Assets/Scripts/CoinHandler.cs
+++ Assets/Scripts/CoinHandler.cs
@@ -1,29 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CoinHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider other)
     {
-        PlayerHandler p = other.GetComponent<PlayerHandler>();
+        PlayerHandler p = other.GetComponent<PlayerHandler>()
+                       ?? other.GetComponentInParent<PlayerHandler>();
         if (p != null)
         {
             p.addCoin();
             HUDManager.Instance?.AddCoin();
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
     }
 }
